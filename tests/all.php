a2e45664e400e1141a87cd60b4cb0f0c57be2a16
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-10-16 13:51:58 -0500

    Some basic tests

---
+++ tests/all.php
@@ -0,0 +1,33 @@
+<?php
+
+require dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+
+chdir('tests');
+mkdir('x/y/z', 0777, TRUE);
+mkdir('a/b/c', 0777, TRUE);
+
+IO\File::open('a/b/z', 'w+', function ($resource) {
+  fwrite($resource, "x\ny");
+});
+
+IO\Dir::cpfiles('a/b', 'x', '*');
+
+IO\File::each('a/b/z', function ($text, $line) {
+  echo " #line $line => $text\n";
+});
+
+IO\Dir::each('.', '*', function ($file) {
+  echo " #file => $file\n";
+});
+
+
+$tmp = IO\Dir::findfile('a/b', '*', TRUE, 1);
+$new = IO\File::read('a/b/z');
+$old = IO\File::read('x/z');
+$len = IO\Dir::size('.');
+
+IO\Dir::unfile('a', '*', TRUE);
+IO\Dir::unfile('x', '*', TRUE);
+
+var_dump(IO\Dir::entries('.', '*', TRUE), $tmp, $new, $old, $len, IO\Helpers::fmtsize($len));
